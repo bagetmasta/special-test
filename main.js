6e7c5af4372bd848a05df1d602c7b134cc6f5fa5
Artem Bahmet <baget.masta@gmail.com>
2023-10-09 15:13:23 +0300

    save final project

--- main.js
+++ main.js
@@ -1,12 +1,41 @@
-function toggleMenu() {
+function adjustNavDisplay() {
+  $(window).on("resize", function () {
+    if ($(window).width() > 767) {
+      $(".nav, .nav__submenu").removeAttr("style");
+      $(".nav__item").off("click");
+      toggleSubmenu();
+    } else {
+      $(".nav").css("display", "none");
+    }
+  });
+}
 
+function toggleSubmenu() {
+  $(".nav__item").on("click", function (e) {
+    if ($(window).width() <= 767) {
+      $(this).children(".nav__submenu").slideToggle();
+      $(this).siblings().find(".nav__submenu").slideUp();
+      e.preventDefault();
+    }
+  });
+}
+
+function toggleMenu() {
+  $(".header__menu-toggle").on("click", function () {
+    $(".header__nav").slideToggle();
+  });
 }
 
 function accordion() {
-
+  $(".accordion__title").on("click", function () {
+    $(this).next(".accordion__description").slideToggle();
+    $(this).parent().siblings().find(".accordion__description").slideUp();
+  });
 }
 
-$(document).ready(function() {
-    toggleMenu();
-    accordion();
-})
+$(document).ready(function () {
+  adjustNavDisplay();
+  toggleMenu();
+  accordion();
+  toggleSubmenu();
+});
